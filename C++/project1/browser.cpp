4c55e5fdb9bba264382e94a43d38f40e8fe0bb3b
koozgur <120639327+koozgur@users.noreply.github.com>
2023-05-18 21:55:51 +0300

    implementation of Browser class

---
+++ C++/project1/browser.cpp
@@ -0,0 +1,105 @@
+#include "browser.h"
+
+Browser::Browser() {
+    windows.append(Window());
+}
+
+void Browser::newWindow() {
+    // TODO
+    windows.prepend(Window());
+    return;
+}
+
+void Browser::closeWindow() {
+    // TODO
+    if(windows.isEmpty())
+        return;
+    else{
+        windows.removeNodeAtIndex(0);
+        return;
+    }
+}
+
+void Browser::switchToWindow(int index) {
+    // TODO
+    if(index < 0)
+        return;
+    windows.moveToIndex(index,0);
+    return;
+}
+
+Window &Browser::getWindow(int index) {
+    return windows.getNodeAtIndex(index) -> data;
+}
+
+void Browser::moveTab(Window &from, Window &to) {
+    // TODO
+    to.newTab(from.getActiveTab());
+    from.closeTab();
+    return;
+}
+
+void Browser::mergeWindows(Window &window1, Window &window2) {
+    // TODO
+    while(!(window2.isEmpty())){
+        window2.changeActiveTabTo(0);
+        window1.newTab(window2.getActiveTab());
+        window2.closeTab();
+    }
+    return;
+}
+
+void Browser::mergeAllWindows() {
+    // TODO
+    Node <Window> * first = windows.getFirstNode();
+    Node <Window> * mover;
+    if(first == NULL)
+        return;
+    if(first -> next == first)
+        return;
+    mover = first -> next;
+    while(mover != first){
+        mergeWindows(first -> data,mover -> data);
+        mover = mover -> next;
+    }
+    return;
+}
+
+void Browser::closeAllWindows() {
+    // TODO
+    windows.removeAllNodes();
+    return;
+}
+
+
+void Browser::closeEmptyWindows() {
+    // TODO
+    Node <Window> * mover = windows.getFirstNode();
+    int counter = 0;
+    int l_size = windows.getSize();
+    Node <Window> * shadow;
+    while(counter < l_size){
+        shadow = mover -> next;
+        if((mover -> data).isEmpty()){
+            windows.removeNode(mover);
+        }
+        counter ++;
+        if(counter < l_size)
+            mover = shadow;
+    }
+    return;
+}
+
+void Browser::print() {
+    Node<Window> *head = windows.getFirstNode();
+    if(head == NULL) {
+        std::cout << "The browser is empty" << std::endl;
+    } else {
+        (head -> data).print();
+        head = head -> next;
+        while(head != windows.getFirstNode()) {
+            (head -> data).print();
+            head = head -> next;
+        }
+    }
+}
